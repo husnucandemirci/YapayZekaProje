450f8ece3904df84b66578487cd7f1cfcd220760
Hüsnücan Demirci <hsn__1@hotmail.com>
2016-12-14 00:31:40 +0300

    Add files via upload

---
+++ BackTracking/BackTracking/BackTrack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTracking
+{
+    public class BackTrack
+    {
+        public static List<Course> MainMethod(List<Prof> Professors)
+        {
+            List<Course> Courses = new List<Course>();
+            List<Course> ArrangedCourses = new List<Course>();
+            foreach (Prof x in Professors)
+            {
+                foreach (Course y in x.Courses)
+                {
+                    y.PossibleTimes = x.FreeTimes;
+                    y.NumberOfPossibleTimes = x.FreeTimes.Count();
+                    Courses.Add(y);
+                }
+            }
+            return Courses;
+        }
+        public static void Backtracker(List<Course> Courses, int NumberOfArranged)
+        {
+            if (NumberOfArranged == Courses.Count())
+            {
+                return;
+            }
+            for (int i = 0
+
+
+
+        }
+
+
+    }
+}
---
+++ BackTracking/BackTracking/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTracking
+{
+    public class Time
+    {   
+        public int Begin;   // 4 haneli
+        public int End;     // 4 haneli
+        public int Day;     // 1, 2, 3, 4 veya 5 olacak
+    }
+}
