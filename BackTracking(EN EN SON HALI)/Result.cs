0246b00fae534c2a5438f11320eec5fd50cf6b4d
Selen ADALI <selenadali94@gmail.com>
2016-12-22 21:49:33 +0200

    Create Result.cs
    Result.cs'e NotArrangedCourses eklendi

---
+++ BackTracking(EN EN SON HALI)/Result.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackTracking
+{
+    public class Result
+    {
+        public List<Course> Courses = new List<Course>();
+        public List<Week> Weeks = new List<Week>();
+        public List<Course> NotArrangedCourses = new List<Course>();
+    }
+}
